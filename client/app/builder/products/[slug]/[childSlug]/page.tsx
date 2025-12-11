2e8894cd1764c8654406d4baa2054412d36844b9
whashishkumar <wh.ashishkumar@gmail.com>
2025-12-11 10:09:03 +0530

    working on bugs

--- client/app/builder/products/[slug]/[childSlug]/page.tsx
+++ client/app/builder/products/[slug]/[childSlug]/page.tsx
@@ -1,15 +1,32 @@
+import BestsellerProducts from '@/components/builder/BestsellerProducts';
+import CategoryProducts from '@/components/builder/CategoryProducts';
+import Solutions from '@/components/builder/Solutions';
 import GetInTouch from '@/components/commercial/GetInTouch';
 import FeaturedProducts from '@/components/residential/FeaturedProducts';
 import ProductDetailPage from '@/components/residential/ProductDetailPage';
+import { BuilderPageData } from '@/lib/api/builderEndPoints';
 import React, { Suspense } from 'react';
 
-export default function page() {
+const headerProductBestSeller = {
+  heading: 'our Featured',
+  subHeading: 'Products',
+};
+export default async function page() {
+  const bestSellerProducts = await BuilderPageData.bestSellerProducts();
+  const categoryBaseProducts = await BuilderPageData.getCategoryProducts();
+
   return (
     <>
       <Suspense
         fallback={<div className="min-h-[8rem] flex items-center justify-center">Loading...</div>}
       >
         <ProductDetailPage />
+        <BestsellerProducts
+          bestSellerProducts={bestSellerProducts}
+          productHeader={headerProductBestSeller}
+        />
+        <Solutions />
+        <CategoryProducts catgoryProductsList={categoryBaseProducts} />
       </Suspense>
       <GetInTouch />
     </>
