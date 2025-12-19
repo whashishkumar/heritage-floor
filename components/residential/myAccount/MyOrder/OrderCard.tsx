936594e423a70d094bdc5c6e0d38240cb2d200c7
whashishkumar <wh.ashishkumar@gmail.com>
2025-12-19 10:57:56 +0530

    fix changes

--- components/residential/myAccount/MyOrder/OrderCard.tsx
+++ components/residential/myAccount/MyOrder/OrderCard.tsx
@@ -40,12 +40,12 @@ export default function OrderCard({
       </td>
       <td className="p-4">
         <div className="flex flex-col gap-2">
-          <button
+          {/* <button
             onClick={() => handleTrackingOrder(order?.increment_id)}
             className="bg-[#008c99]/90 hover:bg-[#008c99] text-white py-1.5 px-3 rounded text-xs font-medium transition-colors cursor-pointer"
           >
             Track Order
-          </button>
+          </button> */}
           {order.status?.toLowerCase() === 'Canceled'.toLowerCase() ? (
             <button
               onClick={() => handleReOrder(order?.id)}
