bbd5069f88616a47f7b19c1e4be409261f2aef19
whashishkumar <wh.ashishkumar@gmail.com>
2025-12-19 16:50:42 +0530

    fix sale offer

--- components/common/specialDeals/SpecialProductCard.tsx
+++ components/common/specialDeals/SpecialProductCard.tsx
@@ -4,7 +4,7 @@ import { useToast } from '@/components/ui/Tooltip';
 import { useAuth } from '@/context/userAuthContext';
 import { CartEndPoint } from '@/lib/api/cartEndPoints';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CiHeart } from 'react-icons/ci';
 import { IoHeart } from 'react-icons/io5';
 
@@ -12,13 +12,23 @@ export default function SpecialProductCard({ specialProducts }: any) {
   const { data } = specialProducts || {};
   const { showToast } = useToast();
   const baseImageUrl = process.env.NEXT_PUBLIC_IMAGE_PATH_WITHOUT_STORAGE;
-  const [isAddingToCart, setIsAddingToCart] = useState(false);
   const { isAuthenticated } = useAuth();
   const [wishlistStatus, setWishlistStatus] = useState<{ [key: number]: boolean }>({});
+  const [addingToCart, setAddingToCart] = useState<{ [key: number]: boolean }>({});
+
+  useEffect(() => {
+    if (data) {
+      const initialStatus = data.reduce((acc: any, product: any) => {
+        acc[product.id] = product.is_wishlist;
+        return acc;
+      }, {});
+      setWishlistStatus(initialStatus);
+    }
+  }, [data]);
 
   const handleAddToCartProduct = async (id: any) => {
-    if (isAddingToCart) return;
-    setIsAddingToCart(true);
+    if (addingToCart[id]) return;
+    setAddingToCart((prev) => ({ ...prev, [id]: true }));
     try {
       if (isAuthenticated) {
         await CartEndPoint.addItemToCart(id);
@@ -33,12 +43,12 @@ export default function SpecialProductCard({ specialProducts }: any) {
         error?.response?.data?.message || 'Failed to add product to cart. Please try again.';
       showToast(errorMessage, 'error');
     } finally {
-      setIsAddingToCart(false);
+      setAddingToCart((prev) => ({ ...prev, [id]: false }));
     }
   };
+
   const handleWhshlistAdd = async (e: React.MouseEvent<HTMLButtonElement>, productId: number) => {
     e.preventDefault();
-    // Optimistic update for just this product
     setWishlistStatus((prev) => ({ ...prev, [productId]: !prev[productId] }));
     try {
       const wishLitItem = await CartEndPoint.addRemoveListItems(productId);
@@ -46,7 +56,6 @@ export default function SpecialProductCard({ specialProducts }: any) {
       showToast(message);
       window.dispatchEvent(new Event('wishList-update'));
     } catch (error) {
-      // Revert optimistic update on error
       setWishlistStatus((prev) => ({ ...prev, [productId]: !prev[productId] }));
       showToast('Failed to update wishlist', 'error');
     }
@@ -54,8 +63,9 @@ export default function SpecialProductCard({ specialProducts }: any) {
 
   return (
     <section className="wrapper py-12 m-auto poppins-font">
-      <h2 className="text-center text-3xl font-semibold text-red-500 mb-12">Sale has ended!</h2>
-
+      <h2 className="text-center text-3xl font-semibold text-red-500 mb-12">
+        Last chance! Sale ending soon.
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {data?.map((product: any) => (
           <div key={product.id} className="bg-white overflow-hidden rounded-t-xl">
@@ -66,26 +76,28 @@ export default function SpecialProductCard({ specialProducts }: any) {
                 fill
                 className="object-cover"
               />
-              {/* {product.waterproof && ( */}
-              <span className="absolute bottom-3 right-3 bg-blue-600 text-white text-xs px-3 py-1 rounded-full">
-                Water Proof
+              <span className="absolute bottom-3 right-3 bg-red-500 text-white text-xs px-3 py-1 rounded-full">
+                Limited Stock
               </span>
-              {/* )} */}
             </div>
             <div className="space-y-2 p-4">
               <h3 className="text-lg font-semibold text-[#0B3A53]">{product.name}</h3>
               <div className="flex items-center gap-2 text-orange-400">
                 {'★'.repeat(product.rating)}
               </div>
-              <div className="space-y-1">
-                <p className="text-gray-400 text-xl font-bold line-through">$ {product.price}</p>
-                <p className="text-green-600 text-2xl font-extrabold">$ {product.special_price}</p>
+              <div className="space-y-1 flex justify-between">
+                <div>
+                  <p className="text-gray-400 text-xl font-bold line-through">$ {product.price}</p>
+                  <p className="text-green-600 text-2xl font-extrabold">
+                    $ {product.special_price}
+                  </p>
+                </div>
                 {isAuthenticated && (
                   <button
                     onClick={(e) => handleWhshlistAdd(e, product?.id)}
                     className={` hover:cursor-pointer py-2 px-2 rounded-2xl text-lg mb-4  font-semibold transition-colors duration-200`}
                   >
-                    {wishlistStatus ? (
+                    {wishlistStatus[product.id] ? (
                       <IoHeart size={30} className="text-[#018C99]" />
                     ) : (
                       <CiHeart size={30} />
@@ -95,12 +107,12 @@ export default function SpecialProductCard({ specialProducts }: any) {
               </div>
               <div className="mt-[1.5rem]">
                 <ButtonCommon
-                  buttonName={isAddingToCart ? 'Adding...' : 'Add To Cart'}
+                  buttonName={addingToCart[product.id] ? 'Adding...' : 'Add To Cart'}
                   image="/icon/arrowRightUp.png"
                   cssParent="!rounded-[0.625rem]"
                   cssChild="!rounded-r-[0.625rem]"
                   onClick={() => handleAddToCartProduct(product?.id)}
-                  disabled={isAddingToCart}
+                  disabled={addingToCart[product.id]}
                 />
               </div>
             </div>
